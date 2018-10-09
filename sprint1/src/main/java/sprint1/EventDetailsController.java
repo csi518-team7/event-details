76a2ebab22cae09cce6c4010641332a13ece0d17
Santhosh Ranganathan <sranganathan2@albany.edu>
2018-10-09 18:55:00 -0400

    Renamed Controller

--- sprint1/src/main/java/sprint1/EventDetailsController.java
+++ sprint1/src/main/java/sprint1/EventDetailsController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Controller {
+public class EventDetailsController {
 
-    @GetMapping("/")
-    public String renderIndex() {
-        return "index";
+    @GetMapping("/add")
+    public String add() {
+        return "Add";
     }
     
 }
