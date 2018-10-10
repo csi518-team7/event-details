5ea0947327968313dee198bb04bf8c1d4809e902
Santhosh Ranganathan <sranganathan2@albany.edu>
2018-10-10 00:04:42 -0400

    Added modify to Controller

--- sprint1/src/main/java/sprint1/EventDetailsController.java
+++ sprint1/src/main/java/sprint1/EventDetailsController.java
@@ -5,11 +5,11 @@ import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.MalformedURLException;
 import java.text.ParseException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -74,6 +74,54 @@ public class EventDetailsController {
         repository.save(new EventDetails(name, description, location, organizerId, thumbnailImageURL, imageURL, startDate, endDate, totalTickets, ticketPrice));
         return "success";
     }
+
+    @GetMapping("/modify-event")
+    public String modify(
+        @RequestParam(name="id", required=true)
+        long id,
+        @RequestParam(name="name", required=true)
+        String name,
+        @RequestParam(name="description", required=false, defaultValue="")
+        String description,
+        @RequestParam(name="location", required=false, defaultValue="")
+        String location,
+        @RequestParam(name="organizerId", required=false, defaultValue="0")
+        long organizerId,
+        @RequestParam(name="thumbnailImageURL", required=false, defaultValue="")
+        URL thumbnailImageURL,
+        @RequestParam(name="imageURL", required=false, defaultValue="")
+        URL imageURL,
+        @RequestParam(name="startDate", required=true)
+        Date startDate,
+        @RequestParam(name="endDate", required=true)
+        Date endDate,
+        @RequestParam(name="totalTickets", required=true)
+        int totalTickets,
+        @RequestParam(name="ticketPrice", required=true)
+        float ticketPrice,
+        Model model) throws MalformedURLException, ParseException {
+
+        model.addAttribute("id", id);
+        try {
+            repository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            return "failed";
+        }
+        model.addAttribute("name", name);
+        model.addAttribute("description", description);
+        model.addAttribute("location", location);
+        model.addAttribute("organizerId", organizerId);
+        model.addAttribute("thumbnailImageURL", thumbnailImageURL);
+        model.addAttribute("imageURL", imageURL);
+        model.addAttribute("startDate", startDate);
+        model.addAttribute("endDate", endDate);
+        model.addAttribute("totalTickets", totalTickets);
+        model.addAttribute("ticketPrice", ticketPrice);
+
+        repository.save(new EventDetails(name, description, location, organizerId, thumbnailImageURL, imageURL, startDate, endDate, totalTickets, ticketPrice));
+        return "success";
+    }
+    
     
     @GetMapping("/show-all-events")
     public String showAllEvents() throws JsonProcessingException {
